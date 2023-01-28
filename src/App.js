0c66a7f6327076485638607f61aabee4875644d0
SoRa0000 <d.bayasgalan1230@gmail.com>
2023-01-28 19:29:34 +0300

    portfolio-2023

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Portfolio 2023
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
