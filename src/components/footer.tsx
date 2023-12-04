9df82b09f11715eecefcd47fcf01fbf16a4aac98
Bayasgalan Davaanyam <d.bayasgalan1230@gmail.com>
2023-12-04 09:38:10 +0300

    deleted .env

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,4 @@
-import React, { FC } from "react";
-
-const Footer: FC = () => {
+const Footer = () => {
     return (
         <footer className="flex bg-dark justify-center items-center font-poppins">
             <div className="flex flex-col w-[800px] items-center justify-center text-slate-300 py-4 text-xs">
